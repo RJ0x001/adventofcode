5aa052564e6162b61298fce76d3be36f071c26b3
rj0x001 <artem.e.pavlenko@gmail.com>
2021-12-07 19:23:02 +0300

    day 5 tasks

--- Day4/python/giant_squid.py
+++ Day4/python/giant_squid.py
@@ -1,4 +1,5 @@
 import copy
+import pprint
 
 
 def read_data(file_path: str) -> tuple:
@@ -14,7 +15,7 @@ def read_data(file_path: str) -> tuple:
     return result_tuple
 
 
-def check_win(d_r: dict, d_c: dict):
+def check_first_win(d_r: dict, d_c: dict):
     for m_k, m in d_r.items():
         for k, v in m.items():
             if False not in v:
@@ -24,56 +25,20 @@ def check_win(d_r: dict, d_c: dict):
             if False not in v:
                 return m_k, k, 'c'
 
-
-def bingo(bingo_numbers, bingo_data):
-    c = 0
-    i = 0
-    grid_list = [[]]
-    for x in bingo_data:
-        if x != '':
-            if c < 5:
-                grid_list[i].append([r for r in x.split()])
-                c += 1
-            else:
-                c = 1
-                i += 1
-                grid_list.append([[r for r in x.split()], ])
-    transpose_grid = []
-    for m in grid_list:
-        transpose_grid.append([[m[j][i] for j in range(len(m))] for i in range(len(m[0]))])
-    res_dict_row = dict()
-    res_dict_col = dict()
-    for m in range(len(grid_list)):
-        res_dict_row[m] = dict()
-        res_dict_col[m] = dict()
-        for c in range(5):
-            res_dict_row[m][c] = [False] * 5
-            res_dict_col[m][c] = [False] * 5
-
-    for n in bingo_numbers:
-        for matrix_num in range(len(grid_list)):
-            for r in range(5):
-                for c in range(5):
-                    if grid_list[matrix_num][r][c] == n:
-                        res_dict_row[matrix_num][r][c] = True
-                    if transpose_grid[matrix_num][r][c] == n:
-                        res_dict_col[matrix_num][r][c] = True
-        win = check_win(res_dict_row, res_dict_col)
-        if win:
-            return count_prize(win[2], int(win[1]), win[0], grid_list, transpose_grid, (res_dict_row, res_dict_col),
-             n)
+def check_last_win():
+    pass
 
 
 def count_prize(pos, pos_number, matrix_key, grid, transpose, marked_dict_tuple, win_number):
+    print(pos, pos_number, matrix_key, win_number)
     if pos == 'r':
         wining_matrix = copy.deepcopy(grid[matrix_key])
         marked_dict = marked_dict_tuple[0]
         for k, v in marked_dict[matrix_key].items():
-            if k.startswith('r'):
-                for i, status in enumerate(v):
+            for i, status in enumerate(v):
                     if status:
-                        elem = grid[matrix_key][int(k)][i]
-                        wining_matrix[int(k[-1])].remove(elem)
+                        elem = grid[matrix_key][k][i]
+                        wining_matrix[k].remove(elem)
         wining_matrix = [int(a) for b in wining_matrix for a in b]
         return sum(wining_matrix) * int(win_number)
     else:
@@ -87,9 +52,64 @@ def count_prize(pos, pos_number, matrix_key, grid, transpose, marked_dict_tuple,
         wining_matrix = [int(a) for b in wining_matrix for a in b]
         return sum(wining_matrix) * int(win_number)
 
+def bingo(bingo_numbers, bingo_data):
+    c = 0
+    i = 0
+    wining_order = []
+    win_condition = []
+    grid_list = [[]]
+    for x in bingo_data:
+        if x != '':
+            if c < 5:
+                grid_list[i].append([r for r in x.split()])
+                c += 1
+            else:
+                c = 1
+                i += 1
+                grid_list.append([[r for r in x.split()], ])
+    transpose_grid = []
+    for m in grid_list:
+        transpose_grid.append([[m[j][i] for j in range(len(m))] for i in range(len(m[0]))])
+    res_dict_row, res_dict_col, marked_stat_dict = dict(), dict(), dict()
+    for m in range(len(grid_list)):
+        res_dict_row[m] = dict()
+        res_dict_col[m] = dict()
+        for c in range(5):
+            res_dict_row[m][c] = [False] * 5
+            res_dict_col[m][c] = [False] * 5
+    for n in bingo_numbers:
+        for matrix_num in range(len(grid_list)):
+            for r in range(5):
+                for c in range(5):
+                    if grid_list[matrix_num][r][c] == n:
+                        res_dict_row[matrix_num][r][c] = True
+                    if transpose_grid[matrix_num][r][c] == n:
+                        res_dict_col[matrix_num][r][c] = True
+        marked_stat_dict[n] = {'r': copy.deepcopy(res_dict_row), 'c': copy.deepcopy(res_dict_col)}
+
+        # pprint.pprint(marked_stat_dict["99"])
+        # first task
+        # 
+        # win = check_first_win(res_dict_row, res_dict_col)
+        # if win:
+        #     return count_prize(win[2], int(win[1]), win[0], grid_list, transpose_grid, (res_dict_row, res_dict_col),
+        #      n)
+        # second task
+        # 
+        
+        win = check_first_win(res_dict_row, res_dict_col)
+        if win:
+            if win[0] not in wining_order:
+                wining_order.append(win[0])
+                win_condition.append((win, n), )
+    win = win_condition[-1]
+    pprint.pprint(marked_stat_dict[win[1]]['r'])
+    return count_prize(win[0][2], int(win[0][1]), win[0][0], grid_list, transpose_grid,
+     (marked_stat_dict[win[1]]['r'], marked_stat_dict[win[1]]['c']), win[1])
+
 
 
 if __name__ == "__main__":
     data = read_data("..\input\input.txt")
     numbers = data[0].split(',')
-    bingo(numbers, data[2:])
+    print(bingo(numbers, data[2:]))
---
+++ Day5/python/hydrothermal_venture.py
@@ -0,0 +1,89 @@
+import pprint
+import copy
+from time import sleep
+
+
+def read_data(file_path: str) -> tuple:
+    """
+    Read task input data and create list for solve
+    Args:
+        file_path (str): Path to input file
+    Returnes:
+        [tuple]: result tuple of input data
+    """
+    with open(file_path, 'r') as file:
+        result_tuple = tuple(x.strip().split('->') for x in file.readlines())
+
+    return result_tuple
+
+
+def mark_dot(c1, c2, counter, grid, overlap_set):
+    try:
+        grid[c1][c2] += 1
+        if (c1, c2) not in overlap_set:
+            overlap_set.add((c1, c2))
+            counter += 1
+    except TypeError:
+        grid[c1][c2] = 1
+    return counter
+
+
+def count_overlap(data):
+    counter = 0
+    overlap_set = set()
+    grid = [[None for _ in range(1000)] for _ in range(1000)]
+    # grid = [[None for _ in range(10)] for _ in range(10)]
+    for row in data:
+        y1, x1 = map(int, row[0].split(','))
+        y2, x2 = map(int, row[1].split(','))
+        
+        if x1 == x2 or y1 == y2:
+            
+            if x1 == x2:
+                if y2 <= y1:
+                    while y2 <= y1:
+                        counter = mark_dot(x2, y2, counter, grid, overlap_set)
+                        y2 += 1
+                else:
+                    while y2 >= y1:
+                        counter = mark_dot(x2, y2, counter, grid, overlap_set)
+                        y2 -= 1
+            else:
+                if x1 <= x2:
+                    while x1 <= x2:
+                        counter = mark_dot(x2, y2, counter, grid, overlap_set)
+                        x2 -= 1
+                else:
+                    while x1 >= x2:
+                        counter = mark_dot(x2, y2, counter, grid, overlap_set)
+                        x2 += 1
+        # task 2
+        else:
+            if x1 <= x2 and y1 >= y2:
+                while x1 <= x2 and y1 >= y2:
+                    counter = mark_dot(x1, y1, counter, grid, overlap_set)
+                    x1 += 1
+                    y1 -= 1
+            elif x1 >= x2 and y1 <= y2:
+                while x1 >= x2 and y1 <= y2:
+                    counter = mark_dot(x1, y1, counter, grid, overlap_set)
+                    x1 -= 1
+                    y1 += 1
+            elif x1 >= x2 and y1 >= y2:
+                while x1 >= x2 and y1 >= y2:
+                    counter = mark_dot(x1, y1, counter, grid, overlap_set)
+                    x1 -= 1
+                    y1 -= 1
+            elif x1 <= x2 and y1 <= y2:
+                while x1 <= x2 and y1 <= y2:
+                    counter = mark_dot(x1, y1, counter, grid, overlap_set)
+                    x1 += 1
+                    y1 += 1
+    # pprint.pprint(grid)
+    return counter
+
+
+if __name__ == "__main__":
+    raw_data = read_data("../input/input.txt")
+    # raw_data = read_data("../input/input copy.txt")
+    pprint.pprint(count_overlap(raw_data))
